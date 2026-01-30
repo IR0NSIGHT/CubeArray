81a899d653affb40a59d0ba99c226dd381d891ac
IR0NSIGHT <irnsght@googlemail.com>
2026-01-30 12:26:42 +0100

    update make exe

--- make_exe.sh
+++ make_exe.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
-jpackage     --input target/      --name CubeArray    --main-jar CubeArray-1.0-SNAPSHOT.jar     --main-class org.ironsight.CubeArray.CubeArrayMain     --type app-image --dest dist/
+# run on windows using git bash. sorry, its shit i know
+# delete old exe
+rm -rf dist
+
+# clean and rebuild
+mvn clean package
+
+# make new exe
+jpackage \
+  --input target/ \
+  --name CubeArray \
+  --main-jar CubeArray-1.0-SNAPSHOT.jar \
+  --main-class org.ironsight.CubeArray.CubeArrayMain \
+  --type app-image \
+  --dest dist/
+
+# zip the app-image
+cd dist && zip -r CubeArray.zip CubeArray && cd ..
+
+# open containing folder (best effort)
+start dist\\CubeArray\\
