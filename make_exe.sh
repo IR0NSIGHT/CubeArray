a6671bfe76717845c5ed15de15eec3b7724dd0a1
IR0NSIGHT <irnsght@googlemail.com>
2026-02-10 18:15:53 +0100

    pin artifact name
    indepentend from version, for make_exe.sh

--- make_exe.sh
+++ make_exe.sh
@@ -10,10 +10,12 @@ mvn clean package
 jpackage \
   --input target/ \
   --name CubeArray \
-  --main-jar CubeArray-1.0-SNAPSHOT.jar \
+  --main-jar CubeArray.jar \
   --main-class org.ironsight.CubeArray.CubeArrayMain \
   --type app-image \
   --dest dist/
+
+mkdir dist
 
 # zip the app-image
 cd dist && zip -r CubeArray.zip CubeArray && cd ..
