8a0ecd4f390f6c67ca024e6a7b716998c809de2c
IR0NSIGHT <irnsght@googlemail.com>
2026-02-13 13:13:11 +0100

    Feature/8 display nbt infos (#13)
    * load schematics in background, display schematic dimension in table
    
    schematics are gradually loaded by background thread
    use Column enum to organize columns + types + renderers
    
    * show x,y,z and diagonal for dimension in table
    
    * add attributes as column
    
    * fix height/depth inverted values
    
    * show column entities, tile_entities

--- src/main/java/org/ironsight/CubeArray/swing/FileTableModel.java
+++ src/main/java/org/ironsight/CubeArray/swing/FileTableModel.java
@@ -1,13 +1,126 @@
 package org.ironsight.CubeArray.swing;
 
+import org.ironsight.CubeArray.PeriodicChecker;
+import org.ironsight.CubeArray.SchemReader;
+import org.joml.Vector3f;
+import org.jspecify.annotations.Nullable;
+import org.pepsoft.minecraft.AbstractNBTItem;
+import org.pepsoft.worldpainter.layers.bo2.Schem;
+import org.pepsoft.worldpainter.objects.WPObject;
+
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
-import java.util.List;
+import java.nio.file.InvalidPathException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class FileTableModel extends AbstractTableModel {
+    private final static DefaultTableCellRenderer fileSizeRenderer = new DefaultTableCellRenderer() {
+        @Override
+        protected void setValue(Object value) {
+            if (value instanceof Long bytes) {
+                setText(formatSize(bytes));
+            } else {
+                setText("");
+            }
+        }
+    };
+
+    private final static DefaultTableCellRenderer stringListRenderer = new DefaultTableCellRenderer() {
+        @Override
+        protected void setValue(Object value) {
+            if (value instanceof List<?> list) {
+                setText(list.stream().filter(Objects::nonNull).map(Object::toString).collect(Collectors.joining(", ")));
+            } else {
+                setText("");
+            }
+        }
+    };
+
+    private final static DefaultTableCellRenderer attributesRenderer = new DefaultTableCellRenderer() {
+        @Override
+        protected void setValue(Object value) {
+            if (value instanceof HashMap<?, ?> map) {
+                setText(
+                        map.entrySet().stream()
+                                .filter(Objects::nonNull)
+                                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                                .sorted()
+                                .collect(Collectors.joining(", "))
+                );
+            } else {
+                setText("");
+            }
+        }
+    };
+
+    public static int NO_VALUE = -1;
+    private final static DefaultTableCellRenderer dimensionRenderer = new DefaultTableCellRenderer() {
+
+        @Override
+        protected void setValue(Object value) {
+            if (value instanceof Integer dim) {
+                if (dim == NO_VALUE) {
+                    setText("?");
+                } else {
+                    setText(dim.toString());
+                }
+            } else {
+                setText("");
+            }
+        }
+    };
     private final List<File> files = new java.util.ArrayList<>();
+    private final HashMap<File, WPObject> schematicObjects = new HashMap<>();
+
+    public FileTableModel(PeriodicChecker checker) {
+        if (checker != null)
+            checker.addCallback(this::tryLoadSchematics);
+    }
+
+    /**
+     * this method will be called from another thread
+     */
+    public void tryLoadSchematics() {
+        List<File> copyFiles;
+        synchronized (files) {
+            copyFiles = List.copyOf(files);
+        }
+
+        // check and load each files schematic if necessary.
+        for (int i = 0; i < copyFiles.size(); i++) {
+            File f = copyFiles.get(i);
+            if (schematicObjects.containsKey(f))
+                continue;
+
+            try {
+                var schems = SchemReader.loadSchematics(List.of(f.toPath()));
+                for (WPObject schem : schems)
+                    schematicObjects.put(f, schem);
+                final int ii = i;
+                SwingUtilities.invokeLater(() -> {
+                    fireTableRowsUpdated(ii, ii);
+                });
+            } catch (IOException | InvalidPathException ex) {
+                System.err.println("unable to load schematic from file: " + f);
+            }
+        }
+
+    }
+
+    private static String formatSize(long bytes) {
+        double kb = bytes / 1024.0;
+        if (kb < 1024) return String.format("%.1f KB", kb);
+
+        double mb = kb / 1024.0;
+        if (mb < 1024) return String.format("%.1f MB", mb);
+
+        double gb = mb / 1024.0;
+        return String.format("%.1f GB", gb);
+    }
 
     @Override
     public int getRowCount() {
@@ -16,36 +129,84 @@ class FileTableModel extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        return 5;
+        return Column.values().length;
     }
 
     @Override
     public Object getValueAt(int row, int col) {
         assert row >= 0 : "row to small: " + row;
         assert row < files.size() : "row to big: " + row + ", " + files.size();
+
+        if (col >= Column.values().length)
+            return null;
+
         File f = files.get(row);
+        WPObject obj = getSchematicFor(f);
+        return switch (Column.values()[col]) {
+            case FILE -> f.getName();
+            case PATH -> f.getAbsolutePath();
+            case FILE_SIZE -> getSizeBytes(f);
+            case FILE_TYPE -> getFileExtension(f);
+            case LAST_CHANGED -> new Date(f.lastModified());
+            case DIMENSION_WIDTH -> obj == null ? NO_VALUE : obj.getDimensions().x;
+            case DIMENSION_HEIGHT -> obj == null ? NO_VALUE : obj.getDimensions().z;
+            case DIMENSION_DEPTH -> obj == null ? NO_VALUE : obj.getDimensions().y;
+            case DIMENSION_DIAGONAL -> {
+                if (obj == null)
+                    yield NO_VALUE;
+                yield Math.round(new Vector3f(obj.getDimensions().x, obj.getDimensions().y, obj.getDimensions().z).length());
+            }
+            case BLOCKS -> {
+                if (obj == null)
+                    yield List.of();
+                yield obj.getAllMaterials().stream()
+                        .filter(Objects::nonNull)
+                        .map(m -> m.simpleName)
+                        .distinct()
+                        .sorted()
+                        .toList();
+            }
 
-        return switch (col) {
-            case 0 -> f.getName();
-            case 1 -> f.getAbsolutePath();
-            case 2 -> new Date(f.lastModified());   // or format later
-            case 3 -> getFileExtension(f);
-            case 4 -> getSizeBytes(f);
-            default -> null;
+            case ATTRIBUTES -> {
+                if (obj instanceof Schem schematic) {
+                    yield schematic.getAttributes();
+                } else {
+                    yield List.of();
+                }
+            }
+
+            case ENTITIES -> {
+                if (obj != null && obj.getEntities() != null) {
+                    yield obj.getEntities().stream()
+                            .map(e -> e.getId() + " [" +
+                                    Arrays.stream(e.getPos())
+                                            .mapToLong(Math::round)
+                                            .mapToObj(Long::toString).collect(Collectors.joining(", "))
+                                    + "]"
+                            ).distinct().sorted().toList();
+                } else
+                    yield List.of();
+            }
+
+            case TILE_ENTITIES -> {
+                if (obj != null && obj.getTileEntities() != null) {
+                    yield obj.getTileEntities().stream().map(te ->
+                            te.getId() + "[" + te.getX() + "," + te.getY() + "," + te.getZ() + "]"
+                    ).distinct().sorted().toList();
+                } else
+                    yield List.of();
+            }
+
+            default -> {
+                assert false : "incomplete enum";
+                yield null;
+            }
         };
     }
 
-    @Override
-    public Class<?> getColumnClass(int col) {
-        return switch (col) {
-            case 0, 1 -> String.class;
-            case 2 -> Date.class;
-            case 3 -> String.class;
-            case 4 -> Long.class;
-            default -> null;
-        };
+    public WPObject getSchematicFor(File f) {
+        return schematicObjects.get(f);
     }
-
 
     private static long getSizeBytes(File f) {
         try {
@@ -68,14 +229,16 @@ class FileTableModel extends AbstractTableModel {
 
     @Override
     public String getColumnName(int column) {
-        return switch (column) {
-            case 0 -> "File";
-            case 1 -> "Path";
-            case 2 -> "Last Changed";
-            case 3 -> "Type";
-            case 4 -> "Size (MB)";
-            default -> "";
-        };
+        if (column >= Column.values().length)
+            return "";
+        return Column.values()[column].displayName;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int column) {
+        if (column >= Column.values().length)
+            return Object.class;
+        return Column.values()[column].clazz;
     }
 
     public File getFileAt(int row) {
@@ -93,9 +256,42 @@ class FileTableModel extends AbstractTableModel {
         for (File file : files) {
             int i = this.files.indexOf(file);
             this.files.remove(file);
+            this.schematicObjects.remove(file);
 
             if (i >= 0) fireTableRowsDeleted(i, i);
             assert !this.files.contains(file);
+        }
+    }
+
+    enum Column {
+        FILE("File", String.class, null, "Name of the file"),
+        LAST_CHANGED("Last Changed", Date.class, null, "Date when the file was last modified"),
+        FILE_TYPE("File Type", String.class, null, "File extension"),
+        FILE_SIZE("File Size (MB)", Long.class, fileSizeRenderer, "Size of the file"),
+        DIMENSION_WIDTH("Width", Integer.class, dimensionRenderer, "Width of the schematic (meters)"),
+        DIMENSION_HEIGHT("Height", Integer.class, dimensionRenderer, "Height of the schematic (meters)"),
+        DIMENSION_DEPTH("Depth", Integer.class, dimensionRenderer, "Depth of the schematic (meters)"),
+        DIMENSION_DIAGONAL("Diagonal", Integer.class, dimensionRenderer, "Diagonal of the schematic from edge to edge (meters)"),
+        PATH("Path", String.class, null, "Filepath where the file lives"),
+        BLOCKS("Blocks", List.class, stringListRenderer, "Blocktypes that are used in the schematic"),
+        ENTITIES("Entities", List.class, stringListRenderer, "Entities in the schematic"),
+        TILE_ENTITIES("Tile Entities", List.class, stringListRenderer, "Tile Entities in the schematic"),
+        ATTRIBUTES("Attributes", HashMap.class, attributesRenderer, "NBT Attributes attached to the schematic"),
+        ;
+        final String displayName;
+        final Class<?> clazz;
+        final String tooltip;
+        final DefaultTableCellRenderer renderer;
+
+        private Column(String name, Class<?> clazz, @Nullable DefaultTableCellRenderer renderer, String tooltip) {
+            this.tooltip = tooltip;
+            this.displayName = name;
+            this.clazz = clazz;
+            if (renderer == null) {
+                this.renderer = new DefaultTableCellRenderer();
+            } else {
+                this.renderer = renderer;
+            }
         }
     }
 }
