67bd328096bf70461fbdde6f7fddf00ca0d33a2f
Ir0nsight <irnsght@googlemail.com>
2026-07-06 13:00:19 +0200

    two zip approach

--- make_exe.sh
+++ make_exe.sh
@@ -24,8 +24,14 @@ jpackage \
 cp staging/CubeArray.jar dist/CubeArray/CubeArray.jar
 cp release/README.txt dist/CubeArray/README.txt
 
-# zip the app-image
+# zip the full app-image (exe + bundled JRE)
 cd dist && zip -r CubeArray.zip CubeArray && cd ..
+
+# zip jar-only (lightweight, requires Java 17+)
+mkdir -p dist/jar-only
+cp staging/CubeArray.jar dist/jar-only/CubeArray.jar
+cp release/README.txt dist/jar-only/README.txt
+cd dist && zip -j CubeArray-win-jar.zip jar-only/CubeArray.jar jar-only/README.txt && cd ..
 
 # open containing folder (best effort)
 start dist\\CubeArray\\
